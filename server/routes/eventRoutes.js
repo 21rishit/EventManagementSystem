aa0147157687c01a98391a8c9222c33ecd6fbc81
21rishit <prishitosh@gmail.com>
2025-02-08 02:35:56 +0530

    fixed backend

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -1,11 +1,11 @@
-import express from "express";
-import { protect } from "../middleware/authMiddleware.js";
-import { createEvent, getEvents, joinEvent } from "../controllers/eventController.js";
+// import express from "express";
+// import { protect } from "../middleware/authMiddleware.js";
+// import { createEvent, getEvents, joinEvent } from "../controllers/eventController.js";
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post("/create", protect, createEvent);
-router.get("/", getEvents);
-router.post("/:id/join", protect, joinEvent);
+// router.post("/create", protect, createEvent);
+// router.get("/", getEvents);
+// router.post("/:id/join", protect, joinEvent);
 
-export default router;
+// export default router;
